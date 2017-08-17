5e6d271caeef72da4fb147b91de7face7f705308
Toan <T@gmail.com>
2017-08-17 07:56:20 +0700

    Remove public folder code

--- __public/js/main.js
+++
@@ -1,9 +0,0 @@
-jQuery('.clr-username').click(function(){
-   jQuery('.inp-username').val('');
-   jQuery('.inp-username').focus();
-});
-
-jQuery('.clr-password').click(function(){
-    jQuery('.inp-password').val('');
-    jQuery('.inp-password').focus();
-});
