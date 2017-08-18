760518bf62fe59f461d98b0da13cee9bf86920c4
sangit16153 <sangbd@dtn.com.vn>
2017-08-18 18:00:43 +0700

    đâm tên th

--- local/resources/views/back/drug/show.blade.php
+++ local/resources/views/back/drug/show.blade.php
@@ -5,9 +5,9 @@
 
   <div class="row col-lg-12">
     <div class="col-md-6">
-      <p>Mã thuốc: {{ $post->code }}</p>
-      <p>Tên thuốc: {{ $post->name }}</p>
-      <p>Nhóm thuốc:
+      <p><b>Mã thuốc: </b> {{ $post->code }}</p>
+      <p><b>Tên thuốc: </b> {{ $post->name }}</p>
+      <p><b>Nhóm thuốc: </b>
         <?php foreach($groupdrug as $gd) { ?>
         <?php if(isset($post->group_drug_id) && $post->group_drug_id == $gd['id']){ ?>
         {!! link_to('groupdrug/' . $gd['id'], $gd['full_name']) !!}
@@ -15,23 +15,23 @@
           }else{echo '';} ?>
         <?php   }  ?>
       </p>
-      <p>Hoạt chất chính: {{ $post->activeIngredient }}</p>
-      <p>Hàm lượng: {{ $post->content }}</p>
-      <p>Dạng bào chế: {{ $post->design }}</p>
-      <p>Quy cách đóng gói: {{ $post->package }}</p>
-      <p>Đơn vị buôn: {{ $post->donvibuon }}</p>
-      <p>Đơn vị lẻ: {{ $post->donvile }}</p>
-      <p>Hệ số quy đổi: {{ $post->hesoquydoi }}</p>
+      <p><b>Hoạt chất chính: </b>{{ $post->activeIngredient }}</p>
+      <p><b>Hàm lượng: </b>{{ $post->content }}</p>
+      <p><b>Dạng bào chế: </b>{{ $post->design }}</p>
+      <p><b>Quy cách đóng gói: </b>{{ $post->package }}</p>
+      <p><b>Đơn vị buôn: </b>{{ $post->donvibuon }}</p>
+      <p><b>Đơn vị lẻ: </b>{{ $post->donvile }}</p>
+      <p><b>Hệ số quy đổi: </b>{{ $post->hesoquydoi }}</p>
     </div>
 
     <div class="col-md-6">
-      <p>Số đăng ký: {{ $post->registerNumber }}</p>
-      <p>Công ty sản xuất: {{ $post->produceCompany }}</p>
-      <p>Nước sản xuất: {{ $post->produceCountry }}</p>
-      <p>Công ty đăng ký: {{ $post->registerCompany }}</p>
-      <p>Nước đăng ký: {{ $post->registerCountry }}</p>
-      <p>Ghi chú: {{ $post->note }}</p>
-      <p>Ảnh thuốc:
+      <p><b>Số đăng ký: </b>{{ $post->registerNumber }}</p>
+      <p><b>Công ty sản xuất: </b>{{ $post->produceCompany }}</p>
+      <p><b>Nước sản xuất: </b>{{ $post->produceCountry }}</p>
+      <p><b>Công ty đăng ký: </b>{{ $post->registerCompany }}</p>
+      <p><b>Nước đăng ký: </b>{{ $post->registerCountry }}</p>
+      <p><b>Ghi chú: </b>{{ $post->note }}</p>
+      <p><b>Ảnh thuốc: </b>
         <?php $stt=0; ?>
         <div class="row">
         @foreach($post->drug_img as $row)
@@ -42,7 +42,7 @@
         @endforeach
         </div>
       </p>
-      <p>Trạng thái: <?php if($post->status == '1') {echo 'Hoạt động';}else{echo 'Khóa';}?></p>
+      <p><b>Trạng thái: </b><?php if($post->status == '1') {echo 'Hoạt động';}else{echo 'Khóa';}?></p>
     </div>
   </div>
 
